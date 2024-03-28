fbd7be1cdae9fd9bf270f197c64e022b294a955c
simonpcouch <simonpatrickcouch@gmail.com>
2024-03-28 12:20:11 -0500

    make `untick()`ing easier

--- R/tick.R
+++ R/tick.R
@@ -4,7 +4,13 @@ tick <- function(fn, pkg = NULL, ..., .measure = "process", .env = caller_env())
   check_enticked(id)
   arg_match(.measure, values = c("process", "real"))
 
-  fn_unmocked <- env_get(ns_env_safe(pkg) %||% .env, fn)
+  fn_unmocked <-
+    env_get(
+      ns_env_safe(pkg) %||% .env,
+      fn,
+      inherit = TRUE,
+      last = base::topenv()
+    )
 
   ticker <- new_ticker(id, fn_unmocked)
 
@@ -18,24 +24,46 @@ tick <- function(fn, pkg = NULL, ..., .measure = "process", .env = caller_env())
 }
 
 #' @export
-untick <- function(x) {
-  check_ticker(x)
-  ticker_id <- as.character(x)
+untick <- function(x, ...) {
+  UseMethod("untick")
+}
 
-  if (ticker_id %in% names("ticks_")) {
-    cli_abort("Could not find registered ticker {.arg {ticker_id}}")
+#' @export
+untick.default <- function(x, ...) {
+  cli_abort("No known {.fun untick} method for {.obj_type_friendly {x}}.")
+}
+
+#' @export
+untick.character <- function(x, pkg = NULL, ...) {
+  id <- make_id(x, pkg)
+
+  if (id %in% names("ticks_")) {
+    cli_abort("Could not find registered ticker for {.arg fn} {x} and \\
+               {.arg pkg} {pkg}.")
   }
 
-  env_unbind(ticks_, ticker_id)
+  env_unbind(ticks_, id)
 
   invisible(x)
 }
 
-check_ticker <- function(x, arg = caller_arg(x), call = caller_env()) {
-  if (!is_ticker(x)) {
-    cli_abort(
-      "{.arg arg} must be a ticker but is {.obj_type_friendly {x}}",
-      call = call
-    )
+#' @export
+untick.ticker <- function(x, ...) {
+  id <- as.character(x)
+  if (id %in% names("ticks_")) {
+    cli_abort("Could not find registered ticker for {.arg {id}}.")
   }
+
+  env_unbind(ticks_, id)
+
+  invisible(x)
 }
+
+# check_ticker <- function(x, arg = caller_arg(x), call = caller_env()) {
+#   if (!is_ticker(x)) {
+#     cli_abort(
+#       "{.arg arg} must be a ticker but is {.obj_type_friendly {x}}.",
+#       call = call
+#     )
+#   }
+# }
