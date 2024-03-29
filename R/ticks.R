2e1ed6af48daf6b69c24a7111bffc9be6c52f993
simonpcouch <simonpatrickcouch@gmail.com>
2024-03-29 08:53:26 -0500

    make `ticks()` a generic

--- R/ticks.R
+++ R/ticks.R
@@ -1,6 +1,22 @@
 #' @export
-ticks <- function(ticker) {
-  ticks_[[as.character(ticker)]]
+ticks <- function(x, ...) {
+  UseMethod("ticks")
+}
+
+#' @export
+ticks.default <- function(x, ...) {
+  cli_abort("No known {.fun ticks} method for {.obj_type_friendly {x}}.")
+}
+
+#' @export
+ticks.ticker <- function(x, ...) {
+  ticks_[[as.character(x)]]
+}
+
+#' @export
+ticks.character <- function(x, pkg = NULL, ...) {
+  id <- make_id(x, pkg)
+  ticks_[[id]]
 }
 
 # `ticks_` is an internal environment that the package uses to track
